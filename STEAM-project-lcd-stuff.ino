7576f6a104707a7c68f0c4cd1f6269d9bca38324
Shaurya Aher <shauryaaher1200@gmail.com>
2022-07-18 20:06:58 +0530

    LCD prints danger when the sensor detects

--- STEAM-project-lcd-stuff.ino
+++ STEAM-project-lcd-stuff.ino
@@ -5,6 +5,7 @@ LiquidCrystal device(11, 8, 4, 5, 6, 7);
 void setup() {
   // put your setup code here, to run once:
   pinMode(2, INPUT);
+  pinMode(12, OUTPUT);
   device.begin(16, 2);
 }
 
@@ -13,7 +14,9 @@ void loop() {
   device.setCursor(6, 0);
   if(digitalRead(2) == HIGH) {
     device.write("Danger");
+    digitalWrite(12, HIGH);
   } else {
     device.write("Safe  ");
+    digitalWrite(12, LOW);
   }
 }
